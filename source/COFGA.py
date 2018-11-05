11b0903dd5711aff1b89420bdc0b643421dd8c8b
helgehatt <helge.hatteland@gmail.com>
2018-11-05 14:00:28 +0100

    Added MAP and WCEL

--- source/COFGA.py
+++ source/COFGA.py
@@ -5,22 +5,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import tensorflow as tf
 from keras import models, layers, optimizers, losses, applications
 from keras.preprocessing.image import ImageDataGenerator
 
-# def AP(ys, ts):
-#     """
-#         Average precision by category
-#     """
-#     predictions = max(ys, 1)
-#     precision = eq(predictions, ts)
-#     return mean(precision.float())
+def MAP(model, generator):
+    """
+        Mean average precision across all categories
+    """
+    predictions = model.predict_generator(generator, steps=len(generator))
+    predictions = np.argmax(predictions, axis=-1)
+    targets = generator.classes
 
-# def MAP(aps):
-#     """
-#         Mean average precision across all categories
-#     """
-#     return mean(aps)
+    AP = np.zeros(len(generator.class_indices))
+    
+    for c in np.unique(generator.classes):
+    # for c in generator.class_indices.values():
+        AP[c] = np.mean(predictions[targets==c] == c)
+    
+    # AP[np.isnan(AP)] = 0 # or 1?
+
+    return np.mean(AP)
 
 
 class MNet(models.Sequential):
@@ -70,8 +75,8 @@ class RNet(models.Sequential):
         self.add(layers.Flatten())
         self.add(layers.Dense(sizes[-2], activation='relu'))
         self.add(layers.Dense(sizes[-1], activation='sigmoid'))
-rnet = RNet()
-rnet.summary()
+# rnet = RNet()
+# rnet.summary()
     
 # https://keras.io/preprocessing/image/#imagedatagenerator-class
 
@@ -79,7 +84,7 @@ rnet.summary()
 generator_args = {
     'rescale':1./255,
     'validation_split':0.3,
-    # 'rotation_range'=20,
+    # 'rotation_range'=45,
     # 'width_shift_range'=0.2,
     # 'height_shift_range'=0.2,
     # 'horizontal_flip'=True,
@@ -90,29 +95,50 @@ generator = ImageDataGenerator(**generator_args)
 
 flow_args = {
     'directory':'../dataset_v2/train/classes',
-    'target_size':(128, 128)
+    'target_size':(128, 128), #(200, 200) for ResNet50
+    'batch_size':32
 }
 
 train_gen = generator.flow_from_directory(subset='training', **flow_args)
 valid_gen = generator.flow_from_directory(subset='validation', **flow_args)
 
+
+from collections import Counter
+Nm = np.array(sorted(Counter(train_gen.classes).items()))[:,1]
+w0 = np.maximum(Nm.astype(np.float)/train_gen.samples, 0.1)
+w = (1 - w0) / w0
+
+def WeightedCELoss(ys, ts):
+    return 1/(tf.shape(ys) + len(w)) * (w * ys * tf.log(ts) + (1 - ys) * tf.log(1 - ts))
+
 compile_args = {
     'optimizer':optimizers.SGD(lr=0.01, momentum=0.9),
-    'loss':losses.binary_crossentropy,
+    'loss': WeightedCELoss, #losses.binary_crossentropy,
     'metrics':['acc']
 }
 fit_gen_args = {
     'generator':train_gen,
     'validation_data':valid_gen,
-    'steps_per_epoch':train_gen.samples/train_gen.batch_size,
-    'validation_steps':valid_gen.samples/valid_gen.batch_size,
-    'epochs':100, 
+    'steps_per_epoch':len(train_gen),
+    'validation_steps':len(valid_gen),
+    'epochs':2, 
     'class_weight':None
 }
 
 mnet.compile(**compile_args)
 mnet.fit_generator(**fit_gen_args)
 mnet.evaluate()
+
+# Get NaNs indices
+# indices = np.array(range(37))
+# indices[np.isnan(AP)]
+
+# Convert to labels
+# label_map = (train_gen.class_indices)
+# label_map = dict((v,k) for k,v in label_map.items()) #flip k,v
+# predictions = [label_map[k] for k in predictions]
+
+# mnet.save('mnet.h5')
 
 # def eval():
 #     """
